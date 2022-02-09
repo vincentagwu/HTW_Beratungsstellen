b4c17b2accd90abef25f8107c97dc15dffce4442
vincentagwu <vincentagwu@gmail.com>
2022-02-09 17:16:32 +0100

    Added Interaction with huggingface model

--- rest-api/main.py
+++ rest-api/main.py
@@ -1,52 +1,40 @@
-from flask import Flask, render_template, abort, request, jsonify, render_template, send_file
+from flask import Flask, render_template, abort, request, jsonify, render_template, send_file, make_response
 from flask_restful import Api, Resource, abort, reqparse
+from flask_cors import CORS, cross_origin
 from pydantic import BaseModel
 from typing import Dict
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import logging
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import requests
+#import translators as ts
+#from googletrans import Translator
+from deep_translator import (GoogleTranslator,
+                             PonsTranslator,
+                             LingueeTranslator,
+                             MyMemoryTranslator,
+                             YandexTranslator,
+                             DeepL,
+                             QCRI,
+                             single_detection,
+                             batch_detection)
+
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
-# Old 
-class SentimentRequest(BaseModel):
-    text: str
-class SentimentResponse(BaseModel):
-    probabilities: Dict[str, float]
-    sentiment: str
-    confidence: float
 
-mlmodel_put_args = reqparse.RequestParser()
-mlmodel_put_args.add_argument("question", type=str, help="Wie lautet ihre Frage?", required=True)
 
-questions= {}
+logging.basicConfig(filename="hello.log", level=logging.INFO)
 
-def question_not_exist(question_id):
-    if question_id not in questions:
-        abort(404, message="Fragen konnte nicht beantwortet werden!")
+#array =[{"asta": {"Kontakt":{"Öffnungszeiten":"Mo,Di,Do: 07:00 - 15:00 Uhr"}}}, {"bim", {""}}, {"fachschaften", {""}}, {"familienbuero", {""}}, {"frauen_und_gleichstellung", {""}}, {"international_office", {""}}, {"lanadema", {""}}, {"mathecafe", {""}}, {"pruefungsamt", {""}}, {"studienverlaufsberatung", {""}}, {"studieren_mit_behinderung", {""}}, {"studierendensekretariat", {""}}]
 
-def question_exist(question_id):
-    if question_id in questions:
-        abort(404, message="Fragen existiert schon !")
-
-class MLModel(Resource):
-    def get(self, question_id):
-        question_not_exist(question_id)
-        return questions[question_id]
-
-    def put(self, question_id):
-        question_exist(question_id)
-        args = mlmodel_put_args.parse_args()
-        questions[question_id]= args
-        return questions[question_id], 201 
-
-    def delete(self, question_id):
-        question_not_exist(question_id)
-        args = mlmodel_put_args.parse_args()
-        questions[question_id]= args
-        return '', 204 
+API_URL = "https://api-inference.huggingface.co/models/rodrigogelacio/autonlp-department-classification-534915130"
+headers = {"Authorization": "Bearer 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"}
 
 
 # New 
@@ -63,114 +51,41 @@ def sentiment(sentence):
     else:
         return "Negative"
 
-# Old
-api.add_resource(MLModel, "/question/<int:question_id>")
-#api.add_resource(HelloWorld, "/helloworld")
+def question(sentence):
+    translated = GoogleTranslator(source='auto', target='de').translate(sentence)
+    print("translated: " + translated)
+    output = query({"inputs": translated})
+    print(output)
+    
+    logging.debug('Debuggggggg' + str(output))  # will not print anything
+    return output
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
 
 
-@app.route('/', methods=['GET'])
-@app.route('/question/<int:question_id>', methods=['GET'])
-@app.route('/question/<int:question_id>', methods=['PUT'])
-
-
-
-#New 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/question", methods = ["GET","POST"])
+@cross_origin()
 def sentimentRequest():
     if request.method == "POST":
         sentence = request.form['q']
-        sent = sentiment(sentence)
-        output['sentiment'] = sent
+        sent = question(sentence)
+        output['answers'] = sent
         return jsonify(output)
     else:
         sentence = request.args.get('q')
-        sent = sentiment(sentence)
-        print(sentence)
-        output['sentiment'] = sent
+        sent = question(sentence)
+        output['answers'] = sent
         return jsonify(output)
 
-# Old
-
+@app.route('/')
+@cross_origin()
 def index():
     try:
         return  send_file('index.html')
     except Exception as e:
         logging.info(e.args[0])
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-# import config
-# import torch
-# import flask
-# import time
-# from flask import Flask
-# from flask import request
-# from model import BERTBaseUncased
-# import functools
-# import torch.nn as nn
-
-
-# app = Flask(__name__)
-
-# MODEL = None
-# DEVICE = config.DEVICE
-# PREDICTION_DICT = dict()
-
-
-# def sentence_prediction(sentence):
-#     tokenizer = config.TOKENIZER
-#     max_len = config.MAX_LEN
-#     review = str(sentence)
-#     review = " ".join(review.split())
-
-#     inputs = tokenizer.encode_plus(
-#         review, None, add_special_tokens=True, max_length=max_len
-#     )
-
-#     ids = inputs["input_ids"]
-#     mask = inputs["attention_mask"]
-#     token_type_ids = inputs["token_type_ids"]
-
-#     padding_length = max_len - len(ids)
-#     ids = ids + ([0] * padding_length)
-#     mask = mask + ([0] * padding_length)
-#     token_type_ids = token_type_ids + ([0] * padding_length)
-
-#     ids = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
-#     mask = torch.tensor(mask, dtype=torch.long).unsqueeze(0)
-#     token_type_ids = torch.tensor(token_type_ids, dtype=torch.long).unsqueeze(0)
-
-#     ids = ids.to(DEVICE, dtype=torch.long)
-#     token_type_ids = token_type_ids.to(DEVICE, dtype=torch.long)
-#     mask = mask.to(DEVICE, dtype=torch.long)
-
-#     outputs = MODEL(ids=ids, mask=mask, token_type_ids=token_type_ids)
-
-#     outputs = torch.sigmoid(outputs).cpu().detach().numpy()
-#     return outputs[0][0]
-
-
-# @app.route("/predict")
-# def predict():
-#     sentence = request.args.get("sentence")
-#     start_time = time.time()
-#     positive_prediction = sentence_prediction(sentence)
-#     negative_prediction = 1 - positive_prediction
-#     response = {}
-#     response["response"] = {
-#         "positive": str(positive_prediction),
-#         "negative": str(negative_prediction),
-#         "sentence": str(sentence),
-#         "time_taken": str(time.time() - start_time),
-#     }
-#     return flask.jsonify(response)
-
-
-# if __name__ == "__main__":
-#     MODEL = BERTBaseUncased()
-#     MODEL.load_state_dict(torch.load(config.MODEL_PATH))
-#     MODEL.to(DEVICE)
-#     MODEL.eval()
-#     app.run(host="0.0.0.0", port="9999")
--- rest-api/test.py
+++ rest-api/test.py
@@ -1,9 +1,49 @@
-import requests
+#Old
+# import requests
 
-BASE_URL = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE_URL + "question/1", {"question": "Wie kann ich mich von Prüfungen abmelden?"})
+# BASE_URL = "http://127.0.0.1:5000/"
+
+# # response = requests.put(BASE_URL + "question/1", {"question": "Wie kann ich mich von Prüfungen abmelden?"})
+# # print(response.json())
+
+# #response = requests.get(BASE_URL + "question/1")
+# response = requests.get(BASE_URL + "question?q='AAPLV $MA All 3 made nice PEG(Power Earnings Gap) candles on Friday. First brave soldiers to 'go over the top… https://t.co/iloZYVDj0w")
 # print(response.json())
 
-response = requests.get(BASE_URL + "question/1")
-print(response.json())
+
+# new
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+# tokenizer = AutoTokenizer.from_pretrained("rodrigogelacio/autonlp-department-classification-534915130")
+
+# model = AutoModelForSequenceClassification.from_pretrained("rodrigogelacio/autonlp-department-classification-534915130")
+# inputs = tokenizer("Ich bin schwanger, was soll ich tun?", return_tensors="pt")
+# outputs = model(**inputs)
+# print(outputs)
+
+
+
+# from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+# model = AutoModelForSequenceClassification.from_pretrained("rodrigogelacio/autonlp-department-classification-534915130", use_auth_token= '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')
+
+# tokenizer = AutoTokenizer.from_pretrained("rodrigogelacio/autonlp-department-classification-534915130", use_auth_token= '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')
+
+# inputs = tokenizer("Ich bin schwanger, was soll ich tun?", return_tensors="pt")
+
+# outputs = model(**inputs)
+# print(outputs)
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/rodrigogelacio/autonlp-department-classification-534915130"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query({"inputs": "Ich bin schwanger, was soll ich tun?"})
+print(output)
